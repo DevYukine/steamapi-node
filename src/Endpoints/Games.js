d6cd483c850ff3f8efd0d8bd8e615b701cb653db
August (Chris) <27751995+ohlookitsAugust@users.noreply.github.com>
2017-11-15 01:07:29 +0000

    small adjustments to error handling and JSDocs (#1)

--- src/Endpoints/Games.js
+++ src/Endpoints/Games.js
@@ -29,13 +29,13 @@ module.exports = class GameEndpoints extends Endpoint {
 	 * @returns {Promise<Array<string>>} App achievements for ID
 	 */
 	async getGameAchievements(app) {
-		if (isNaN(app)) throw new Error('no appid provided');
+		if (isNaN(app)) throw new Error('There isn\'t a App ID defined.');
 		const { body } = await get(`${this.baseURL}/ISteamUserStats/GetGlobalAchievementPercentagesForApp/v2?gameid=${app}`, { headers: this.headers });
 		return body.achievementpercentages.achievements;
 	}
 
 	/**
-	 * Get details for app id
+	 * Get details for App ID!
 	 * @async
 	 * <warn>Requests for this endpoint are limited to 200 every 5 minutes</warn>
 	 * @param {string} app App ID
@@ -43,7 +43,7 @@ module.exports = class GameEndpoints extends Endpoint {
 	 * @returns {Promise<Object>} App details for ID
 	 */
 	async getGameDetails(app, force) {
-		if (isNaN(app)) throw new Error('no appid provided');
+		if (isNaN(app)) throw new Error('There isn\'t a App ID defined.');
 		app = app.toString();
 		if (this.options.enabled) {
 			if (force || (this.cache.has(app) && this.cache.get(app).expires <= Date.now()) || !this.cache.has(app)) {
@@ -56,7 +56,7 @@ module.exports = class GameEndpoints extends Endpoint {
 			}
 		} else {
 			const { body } = await get(`https://store.steampowered.com/api/appdetails?appids=${app}`, { headers: this.headers });
-			if (!body[app].success) throw new Error('no app found');
+			if (!body[app].success) throw new Error('No App ID found.');
 			return body[app].data;
 		}
 		return this.cache.get(app).data;
@@ -69,9 +69,9 @@ module.exports = class GameEndpoints extends Endpoint {
 	 * @returns {Promise<Array<Object>>} App news for ID
 	 */
 	async getGameNews(app) {
-		if (isNaN(app)) throw new Error('no appid provided');
+		if (isNaN(app)) throw new Error('There isn\'t a App ID defined.');
 		const { body } = await get(`${this.baseURL}/ISteamNews/GetNewsForApp/v2?appid=${app}`, { headers: this.headers });
-		if (body.appnews.count === 0) throw new Error('no news found');
+		if (body.appnews.count === 0) throw new Error('No news found for App ID!');
 		return body.appnews.newsitems;
 	}
 
@@ -82,9 +82,9 @@ module.exports = class GameEndpoints extends Endpoint {
 	 * @returns {Promise<number>} Number of players
 	 */
 	async getGamePlayers(app) {
-		if (isNaN(app)) throw new Error('no appid provided');
+		if (isNaN(app)) throw new Error('There isn\'t a App ID defined.');
 		const { body } = await get(`${this.baseURL}/ISteamUserStats/GetNumberOfCurrentPlayers/v1?appid=${app}`, { headers: this.headers });
-		if (body.response.result !== 1) throw new Error('invalid app id');
+		if (body.response.result !== 1) throw new Error('That was an invalided ID.');
 		return body.response.player_count;
 	}
 
@@ -95,9 +95,9 @@ module.exports = class GameEndpoints extends Endpoint {
 	 * @returns {Promise<number>} Schema
 	 */
 	async getGameSchema(app) {
-		if (isNaN(app)) throw new Error('no appid provided');
+		if (isNaN(app)) throw new Error('There isn\'t a App ID defined.');
 		const { body } = await get(`${this.baseURL}/ISteamUserStats/GetSchemaForGame/v2?key=${this.key}&appid=${app}`, { headers: this.headers });
-		if (!body.game) throw new Error('game not found');
+		if (!body.game) throw new Error('Game not found.');
 		return body.game;
 	}
 };

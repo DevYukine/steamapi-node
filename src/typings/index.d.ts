0348e04eb7a4aedabcf52c2f632053dfe0282fe0
Yukine <blazylp@gmail.com>
2017-11-28 21:54:03 +0100

    fixed typings

--- src/typings/index.d.ts
+++ src/typings/index.d.ts
@@ -1,86 +1,83 @@
 declare module 'steamapi-node'{
 
 	export class SteamUser {
-		constructor(key: String, cacheOptions: cacheOptions, endpoints: Array<Endpoint>)
+		constructor(key: string, cacheOptions: cacheOptions, disabledEndpoints: Array<string>)
 
-		public options: {
-			enabled: Boolean,
-			expires: Number
-		};
+		public options: cacheOptions
 		
 		public others?: OtherEndpoints
 		public games?: GameEndpoints
 		public users?: UserEndpoints
 	}
 
-	export class GameEndpoints extends Endpoint {
-		constructor(key: String, options: cacheOptions)
+	class GameEndpoints extends Endpoint {
+		constructor(key: string, options: cacheOptions)
 
 		public getFeaturedGames(): Promise<Object>
 
-		public getGameAchievements(app: String): Promise<Array<String>>
+		public getGameAchievements(app: string): Promise<Array<string>>
 
-		public getGameDetails(app: String, force: Boolean): Promise<Object>
+		public getGameDetails(app: string, force: Boolean): Promise<Object>
 
-		public getGameNews(app: String): Promise<Array<Object>>
+		public getGameNews(app: string): Promise<Array<Object>>
 
-		public getGamePlayers(app: String): Promise<Number>
+		public getGamePlayers(app: string): Promise<number>
 
-		public getGameSchema(app: String): Promise<Number>
+		public getGameSchema(app: string): Promise<number>
 	}
 
-	export class UserEndpoints extends Endpoint {
-		constructor(key: String, options: cacheOptions)
+	class UserEndpoints extends Endpoint {
+		constructor(key: string, options: cacheOptions)
 
 		public idReg: RegExp
 
-		public getUserAchievements(id: String, app: String): Promise<Object>
+		public getUserAchievements(id: string, app: string): Promise<Object>
 
-		public getUserBadges(id: String): Promise<Array<Object>>
+		public getUserBadges(id: string): Promise<Array<Object>>
 
-		public getUserBans(id: String): Promise<Object>
+		public getUserBans(id: string): Promise<Object>
 
-		public getUserFriends(id: String): Promise<Array<Object>>
+		public getUserFriends(id: string): Promise<Array<Object>>
 
-		public getUsersGroup(id: String): Promise<Array<Object>>
+		public getUsersGroup(id: string): Promise<Array<Object>>
 
-		public getUserLevel(id: String): Promise<Number>
+		public getUserLevel(id: string): Promise<number>
 
-		public getUserOwnedGames(id: String): Promise<Array<Object>>
+		public getUserOwnedGames(id: string): Promise<Array<Object>>
 
-		public getUserRecentGames(id: String): Promise<Array<Object>>
+		public getUserRecentGames(id: string): Promise<Array<Object>>
 
-		public getUserServers(key: String): Promise<Object>
+		public getUserServers(key: string): Promise<Object>
 
-		public getUserStats(id: String, app: String): Promise<Object>
+		public getUserStats(id: string, app: string): Promise<Object>
 
-		public getUserSummary(id: String): Promise<Object>
+		public getUserSummary(id: string): Promise<Object>
 	}
 
-	export class OtherEndpoints extends Endpoint {
-		constructor(key: String, options: cacheOptions)
+	class OtherEndpoints extends Endpoint {
+		constructor(key: string, options: cacheOptions)
 
-		public get(path: String): Promise<Object>
+		public get(path: string): Promise<Object>
 
-		public resolve(info: string): Promise<String>
+		public resolve(info: string): Promise<string>
 
 		public getAppList(): Promise<Array<Object>>
 
 		public getFeaturedCategories(): Promise<Object>
 
-		public getServers(host: String): Promise<Array<Object>>
+		public getServers(host: string): Promise<Array<Object>>
 	}
 
 	class Endpoint {
-		constructor(key: String, options: cacheOptions)
+		constructor(key: string, options: cacheOptions)
 
-		public baseURL: String
-		public key: String
+		public baseURL: string
+		public key: string
 		public headers: {
-			'User-Agent': String
+			'User-Agent': string
 		}
 		public options: cacheOptions
-		public cache?: Map<String, cache>
+		public cache?: Map<string, cache>
 	}
 
 	type cache = {
@@ -89,7 +86,7 @@ declare module 'steamapi-node'{
 	}
 
 	type cacheOptions = {
-		enabled: Boolean,
-		expires: Number 
+		enabled: boolean,
+		expires: number 
 	}
 }

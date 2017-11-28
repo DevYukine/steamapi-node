e906edc82cc08423b389327fb4060a5af8b11c0a
Yukine <blazylp@gmail.com>
2017-11-28 20:05:57 +0100

    added Dim to the license file and also added typings and renamed a variable in SteamUser Class

---
+++ index.d.ts
@@ -0,0 +1,110 @@
+declare module 'SteamApi'{
+
+	export class SteamUser {
+		constructor(key: String, cacheOptions: {
+			enabled: boolean,
+			expires: number
+		}, endpoints: [GameEndpoints, UserEndpoints, OtherEndpoints])
+
+		public options: {
+			enabled: Boolean,
+			expires: Number
+		};
+		
+		public others?: OtherEndpoints
+		public games?: GameEndpoints
+		public users?: UserEndpoints
+	}
+
+	export class GameEndpoints extends Endpoint {
+		constructor(key: String, options: {
+			enabled: Boolean,
+			expires: Number   
+		})
+
+		public getFeaturedGames(): Promise<Object>
+
+		public getGameAchievements(app: String): Promise<Array<String>>
+
+		public getGameDetails(app: String, force: Boolean): Promise<Object>
+
+		public getGameNews(app: String): Promise<Array<Object>>
+
+		public getGamePlayers(app: String): Promise<Number>
+
+		public getGameSchema(app: String): Promise<Number>
+	}
+
+	export class UserEndpoints extends Endpoint {
+		constructor(key: String, options: {
+			enabled: Boolean,
+			expires: Number   
+		})
+
+		public idReg: RegExp
+
+		public getUserAchievements(id: String, app: String): Promise<Object>
+
+		public getUserBadges(id: String): Promise<Array<Object>>
+
+		public getUserBans(id: String): Promise<Object>
+
+		public getUserFriends(id: String): Promise<Array<Object>>
+
+		public getUsersGroup(id: String): Promise<Array<Object>>
+
+		public getUserLevel(id: String): Promise<Number>
+
+		public getUserOwnedGames(id: String): Promise<Array<Object>>
+
+		public getUserRecentGames(id: String): Promise<Array<Object>>
+
+		public getUserServers(key: String): Promise<Object>
+
+		public getUserStats(id: String, app: String): Promise<Object>
+
+		public getUserSummary(id: String): Promise<Object>
+	}
+
+	export class OtherEndpoints extends Endpoint {
+		constructor(key: String, options: {
+			enabled: Boolean,
+			expires: Number   
+		})
+
+		public get(path: String): Promise<Object>
+
+		public resolve(info: string): Promise<String>
+
+		public getAppList(): Promise<Array<Object>>
+
+		public getFeaturedCategories(): Promise<Object>
+
+		public getServers(host: String): Promise<Array<Object>>
+	}
+
+	class Endpoint {
+		constructor(key: String, group: String, options: {
+			enabled: Boolean,
+			expires: Number   
+		})
+
+		public baseURL: String
+		public key: String
+		public headers: {
+			'User-Agent': String
+		}
+		public group: String
+		public options: {
+			enabled: Boolean,
+			expires: Number  
+		}
+		public cache?: Map<String, {
+			date: Object
+			expires: number
+		}>
+
+
+	}
+
+}
--- src/SteamUser.js
+++ src/SteamUser.js
@@ -8,16 +8,16 @@ module.exports = class SteamUser {
 	 * Create a SteamUser Class to interact with the SteamApi
 	 * @class
 	 * @param {string} key Steam key
-	 * @param {Object} [cache={}] Optional options for caching `getGameDetails()`
-	 * @param {boolean} [cache.enabled=true] Whether to cache or not
-	 * @param {number} [cache.expires=86400000] How long cache should last for (ms) [1 day by default]
-	 * @param {Array<Endpoints>} [endpoints = []] An Array of all endpoint groups that this SteamUser should have acess to. [all endpoint groups by default]
+	 * @param {Object} [cacheOptions={}] Optional options for caching `getGameDetails()`
+	 * @param {boolean} [cacheOptions.enabled=true] Whether to cache or not
+	 * @param {number} [cacheOptions.expires=86400000] How long cache should last for (ms) [1 day by default]
+	 * @param {Array<Endpoints>} [endpoints = [endpoints]] An Array of all endpoint groups that this SteamUser should have acess to. [all endpoint groups by default]
 	 */
-	constructor(key, cache = {}, endpoints = [Games, Users, Others]) {
+	constructor(key, cacheOptions = {}, endpoints = [Games, Users, Others]) {
 		if (!key) throw new Error('no key provided | some methods may not work | go get one > https://goo.gl/DfNy5s');
 		this.options = {
-			enabled: cache.enabled !== false,
-			expires: cache.expires || 86400000
+			enabled: cacheOptions.enabled !== false,
+			expires: cacheOptions.expires || 86400000
 		};
 		if (endpoints.some(endpoint => !(endpoint.prototype instanceof EndpointClass))) throw Error('The endpoint array only accept Classes that extend the Endpoint Class.');
 		for (const Endpoint of endpoints) {
